7790020af31ad1ff3a9976fbf3a367524574b545
Koen Werdler <koen@morgenwerk.nl>
2015-08-05 19:40:21 +0200

    Added Bootstrap and some development gems (thin, better_errors and binding_of_caller)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,13 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
+// Note: First load bootstrap and then JQuery UI! This prevents problems with dialog.
+//       More info: http://stackoverflow.com/questions/17367736/jquery-ui-dialog-missing-close-icon
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
+// ## = require jquery-ui/sortable
+// ## = require jquery-ui/autocomplete
 //= require turbolinks
 //= require_tree .
