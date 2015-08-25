134c62c7469cf5a6c814b3fd225e920a79fb6e69
Koen Werdler <koen@morgenwerk.nl>
2015-08-25 22:38:25 +0200

    Added column 'name' to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806200945) do
+ActiveRecord::Schema.define(version: 20150825183113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150806200945) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "video_infos", force: :cascade do |t|
