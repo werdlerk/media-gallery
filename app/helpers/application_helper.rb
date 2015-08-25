d37e9980654dc748e06fb4c7250e086d1a9ad444
Koen Werdler <koen@morgenwerk.nl>
2015-08-25 22:40:21 +0200

    Highlight the correct menu item when the related page is active.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  MENU_ITEMS = {
+    :overview => 'media#index',
+    :media_upload => 'media#new'
+  }
+
+  def menu_active?(menu_item)
+    MENU_ITEMS[menu_item].present? &&
+      ((MENU_ITEMS[menu_item].include?('#') &&
+        MENU_ITEMS[menu_item].split('#')[0] == controller_name &&
+        MENU_ITEMS[menu_item].split('#')[1] == action_name) ||
+       (MENU_ITEMS[menu_item] == controller_name)
+      )
+  end
+
 end
